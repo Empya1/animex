84d77a14bd42b852f07b4747779d4cda1f956a86
Empya1 <161871238+Empya1@users.noreply.github.com>
2024-12-02 23:05:07 +0100

    Update appdata.py

---
+++ appdata.py
@@ -0,0 +1,19 @@
+from markupsafe import Markup
+
+API_URL = "https://empya1.pythonanywhere.com"
+GET = "/get"
+GETALL = "/get_all"
+EDIT = "/edit"
+DELETE = "/delete"
+fb_link = "#"
+wa_link="#"
+x_link="#"
+
+multitag = Markup("""
+""")
+interstitial = Markup("""
+""")
+vignette = Markup("""
+""")
+banners = Markup("""
+""")
